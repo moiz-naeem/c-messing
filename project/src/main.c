bc75c73204d7507c8d878e7a08526c24130b5ad4
MoizNaeem <moiznaeem20@gmail.com>
2025-09-24 14:38:25 +0300

    humdity through sys system

--- project/src/main.c
+++ project/src/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <inttypes.h>
+#include <math.h>>
+
+
+
 int main(void){
+    float humidity(uint16_t reg){
+        double humidity_percent = ((double)reg / pow(2.0, 16.0)) * 100. 0 ; 
+        return float(humidity_percent);
+    }
     printf("Initial C setup");
+    uint16_t arg = 0x8000;
+    float hum = humidity(arg);
+    printf("Humidity: %.2f %%\n", hum);
     return 0;
 }
+
